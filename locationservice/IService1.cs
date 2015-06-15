ff6e7bd8c34f6cda2fab306d1e54330fc9ce6b18
hieuntp2 <hieuntp2@gmail.com>
2015-06-15 16:21:37 +0700

    first

---
+++ locationservice/IService1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace locationservice
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface myservice
+    {
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        void reciveMyLocation(List<MyLocation> locations);
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class ListLocation
+    {
+        public List<MyLocation> locations { get; set; }
+    }
+
+    [DataContract]
+    public class MyLocation
+    {
+        [DataMember]
+        public string X { get; set; }
+
+        [DataMember]
+        public string Y { get; set; }
+
+        [DataMember]
+        public string message { get; set; }
+
+        [DataMember]
+        public DateTime time { get; set; }
+    }
+}
---
+++ locationservice/Service1.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using locationservice.Database;
+using System.Web.Http.Cors;
+namespace locationservice
+{
+    public class Service1 : myservice
+    {
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        public void json(List<MyLocation> locations)
+        {
+            if (locations != null)
+            {
+                if (locations.Count > 0)
+                {
+                    locationserviceEntities db = new locationserviceEntities();
+
+                    for (int i = 0; i < locations.Count; i++)
+                    {
+                        location item = new location();
+                        item.X = locations[i].X;
+                        item.Y = locations[i].Y;
+                        item.message =  "[json-method] " + locations[i].message;
+                        item.uploadtime = DateTime.Now;
+                        item.time = locations[i].time;
+                        db.locations.Add(item);
+                    }
+
+                    db.SaveChangesAsync();
+                }
+            }
+        }
+
+        public void normalmethod(string x, string y, string message, DateTime time)
+        {
+            locationserviceEntities db = new locationserviceEntities();
+            location item = new location();
+            item.X = x;
+            item.Y = y;
+            item.message = "[normal-method] " + message;
+            item.uploadtime = DateTime.Now;
+            item.time = time;
+            db.locations.Add(item);
+
+            db.SaveChangesAsync();
+
+        }
+    }
+}
